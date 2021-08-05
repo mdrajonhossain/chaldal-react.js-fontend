8391dd42c0b255e24197e44a69fd227c45aac3b8
rajon hossain <rajon>
2021-08-05 23:07:25 +0600

    test

--- src/component/Foodaid.js
+++ src/component/Foodaid.js
@@ -22,9 +22,132 @@ function Foodaid() {
 	
 
 		<div className="textbody" id="textbody" style={{marginTop:'4%'}}> 
-			<div className="">Foodaid</div>
+			<div className="aidtop">
+				<div className="aidtopleft">
+					<img src="https://chaldn.com/asset/Egg.Grocery.Fabric/Egg.Grocery.Web/1.5.0+DataCenter-Release-2254/Default/stores/chaldal/components/page/Donation/images/zimgo_logo.png?q=best&webp=1"/>
+					<br/><span className="txt1">Donate your old phone <span className="txtred">to feed a family of 4</span></span>
+					<br/>
+					<div className="txt2">Donate your old, idle devices to turn into food for the underprivileged</div>
+					<div className="dxtbutton">DONATE YOUR DEVICE NOW</div>
+				</div>
+
+				<div className="aidtopright">
+					<img src="https://chaldn.com/asset/Egg.Grocery.Fabric/Egg.Grocery.Web/1.5.0+DataCenter-Release-2254/Default/stores/chaldal/components/page/Donation/images/creative.png?q=best&webp=1" style={{height: '200px'}}/>
+				</div>
+			</div>
+
+
+
+				<div className="aidcomverpic">
+					<img src="./image/donation_1.webp" style={{width:'100%', height:'650px'}}/>
+
+
+				<div className="aidcomvertext">
+				<span className="welcome">Welcome To</span> <span className="foodaid">Food Aid</span>
+				<br/><span className="aplatform">A platform to connect those with the means to give and those who are in need of help</span>
+					
+				<div className="welcombtn">					
+					<div className="donet_btn">DONATE</div>
+					<div className="reqfood_btn">REQUEST FOOD AID</div>
+				</div>
+
+					
+				</div>
+
+				</div>
+
+				<div className="how_we_help">HOW WE HELP</div>
+
+				<div className="by_building">
+					By building and operating an accountable and sustainable distribution network to connect generous partners with hopeful beneficiaries.
+				</div>
+
+
+				<div className="accept_donet_3col">
+					<div className="">
+						<img src="./image/foodhand.JPG" style={{width:'140px', height:'100px'}}/>
+					</div>					
+					<div className="">
+						<img src="./image/man.JPG" style={{width:'140px', height:'100px'}}/>
+					</div>					
+					<div className="">
+						<img src="./image/pepole.JPG" style={{width:'140px', height:'100px'}}/>
+					</div>
+				</div>
+
+				<div className="baby_comver_pic">			
+					<img src="./image/kids.jpg" style={{height:'300px'}}/>
+					
+					<div className="baby_comver_text">
+
+						Created to finance humanitarian relief operations during the pandemics, Food-Aid provides an efficient logistics solution to ensure that funds are used to provide proper nutrition to those in need of assistance during emergency situations.
+						<br/><br/>
+						We match donations to those requesting aid, maintain transparency with donors and beneficiaries, and audit all donations and requests coming in. We hope to serve families quickly and efficiently, and work diligently towards a hunger-free Bangladesh.
+					</div>
+
+
+					<div className="milion_box">
+						<div className="milion">
+							<span>9.5 million</span><br/>
+							<p>children of pre-school age are stunted from malnutrition.</p>
+						</div>
+
+						<div className="milion">
+							<span>21.8%</span><br/>
+							<p>of the population live below the poverty line.</p>
+						</div>
+
+						<div className="milion">
+							<span>40 million</span><br/>
+							<p>Bangladeshis are food insecure..</p>
+						</div>						
+					</div>
+
+
+					<div className="no_act_kindness">
+						“No act of kindness, no matter how small, is ever wasted.”<br/>- Aesop
+					</div>
+
+
+					<div className="milion_box">
+						<div className="milion">
+							<span>৳3,120</span><br/>
+							<p>feeds a family of 4 for an entire month.</p>
+						</div>
+
+						<div className="milion">
+							<span>৳4,200</span><br/>
+							<p>gives a person food for the whole year..</p>
+						</div>
+
+						<div className="milion">
+							<span>৳1,260</span><br/>
+							<p>feeds a hungry child for an entire month.</p>
+						</div>						
+					</div>
+
+
+					<div className="mission_vission">
+						<div className="mission">
+							<img src="https://chaldn.com/asset/Egg.Grocery.Fabric/Egg.Grocery.Web/1.5.0+DataCenter-Release-2254/Default/stores/chaldal/components/page/Donation/images/banner_1.png" style={{width:'100%', height:'300px'}}/>
+						</div>
+						<div className="vission">
+							<img src="https://chaldn.com/asset/Egg.Grocery.Fabric/Egg.Grocery.Web/1.5.0+DataCenter-Release-2254/Default/stores/chaldal/components/page/Donation/images/banner_2.png" style={{width:'100%',height:'300px'}}/>
+						</div>
+					</div>
+
+
+
+					
+				</div>
+
+
+
+
+
 		</div> 
 	
+				
 
 
 
--- src/component/Offerpage.js
+++ src/component/Offerpage.js
@@ -6,7 +6,11 @@ import Sitebar from './sitebar';
 function City() {
 
  
-	
+	const addtobag = ()=> {
+		alert("asdfasfd");
+	}
+
+
 
 
   return (
@@ -22,7 +26,83 @@ function City() {
 
 
 		<div className="textbody" id="textbody" style={{marginTop:'4%'}}> 
-			<div className="">Officer Page</div>
+
+			<div className="category_filter_title">Filter By Category</div>
+
+			<div className="category_tap_multi">
+				<div className="category_tap">
+					Popular(24)
+					<img src="https://chaldn.com/_mpimage/popular?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D80219&q=low&v=1&m=40&webp=1&alpha=1" />
+				</div>
+				
+				<div className="category_tap">
+					Hyginene(25)
+					<img src="https://chaldn.com/_mpimage/hygiene?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D80229&q=low&v=1&m=40&webp=1&alpha=1" />
+				</div>
+				
+				<div className="category_tap">
+					Baby Care(26)
+					<img src="https://chaldn.com/_mpimage/baby-care?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D80247&q=low&v=1&m=40&webp=1&alpha=1" />
+				</div>
+				
+				<div className="category_tap">
+					Pet Care(27)
+					<img src="https://chaldn.com/_mpimage/pet-care?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D80246&q=low&v=1&m=40&webp=1&alpha=1" />
+				</div>
+				
+				<div className="category_tap">
+					Food(28)
+					<img src="https://chaldn.com/_mpimage/food?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D80241&q=low&v=1&m=40&webp=1&alpha=1" />
+				</div>
+				
+				<div className="category_tap">
+					Home & Cleaning(29)
+					<img src="https://chaldn.com/_mpimage/home-cleaning?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D80249&q=low&v=1&m=40&webp=1&alpha=1" />
+				</div>
+			</div>
+			<hr style={{width:'95%'}}/>
+
+			<div className="special_offer_title">Special Offers</div>
+
+
+
+
+
+
+
+			<div className="special_offer_product">
+				<div className="offer_product_img">
+					<img src="./image/mango-aamropali-net-weight-50-gm-3-kg.webp"/>
+				</div>
+
+
+				<div className="offer_product">
+					<img src="./image/minimango.webp"/>
+					<div className="kg50">Mango Aamrapali + 50kg</div>
+					<div className="kg">3Kg</div>
+					<div className="price">৳ 550/- </div><br/><br/>
+					<div className="add_to_bag" onClick={()=>addtobag()}>Add to Bag</div>
+				</div>
+			</div>
+
+			
+
+			<div className="ss" style={{height:'500px'}}>asdfasdf</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		</div> 
 	
 
